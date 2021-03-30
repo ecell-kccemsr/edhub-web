290b3e0b1f0385d7f23719e681bc481b86af3ed1
prachinimase <prachinimase@gmail.com>
2021-03-30 17:41:04 +0530

    added course and course provider model

---
+++ database/migrations/2021_03_30_121322_create_courses_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('subtitles');
+            $table->string('image');
+            $table->string('url');
+            $table->longText('skills')->nullable();
+            $table->longText('career_outcome')->nullable();
+            $table->longText('job_opportunities')->nullable();
+            $table->longText('description')->nullable();
+            $table->longText('course_content')->nullable();
+            $table->longText('prerequisites')->nullable();
+            $table->string('instructor_name')->nullable();
+            $table->unsignedDouble('instructor_rating')->nullable();
+            $table->unsignedInteger('instructor_reviews')->nullable();
+            $table->unsignedInteger('instructor_students')->nullable();
+            $table->longText('instructor_designation')->nullable();
+            $table->longText('instructor_description')->nullable();
+            $table->longText('FAQs');
+            $table->boolean('certification');
+            $table->unsignedDouble('rating');
+            $table->unsignedDouble('price');
+            $table->unsignedInteger('course_provider_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
