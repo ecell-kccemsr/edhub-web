0cce8f3818435d4ed1bc10d9b8e7f8660854e55f
Shriyash Jadhav <shriyashjadhav370@gmail.com>
2021-04-07 21:28:42 +0530

    phase2 footer added

---
+++ resources/js/layout/footer/FooterCourse.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Container, Row, Col, FormGroup, Input,Button } from "reactstrap";
+
+const Footer = () => {
+    const [email, setEmail] = useState("");
+
+    const handleFooterForm = e => {
+        e.preventDefault();
+        e.target.reset();
+    };
+
+    return (
+        <footer className="footer-course-section pb-5 pt-2">
+            <Container style={{ boxShadow: "none" }}>
+                <Row className="justify-content-center">
+                    <Col sm="12" md="6">
+                        {/* Footer-Title */}
+                        <h2 className="footer-course-header">
+                            Sign up to our Newsletter 
+                        </h2>
+                        <p className="footer-course-paragraph">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Habitant volutpat elementum facilisi mattis et. At 
+                        </p>
+                        {/* Footer-Form */}
+                        <Row className="justify-content-center footer-course-row">
+                            <Col sm="12" md="8">
+                                <form onSubmit={handleFooterForm} >
+                                    <FormGroup style={{display:"flex"}}>
+                                        <Input
+                                        className="footer-course-email-input"
+                                            type="email"
+                                            name="email"
+                                            id="email"
+                                            defaultValue={email}
+                                            onBlur={e =>
+                                                setEmail(e.target.value)
+                                            }
+                                            placeholder="someone@gmail.com"
+                                            
+                                        />
+                                              <Button className="footer-course-btn">Submit</Button>
+
+                                    </FormGroup>
+                                </form>
+
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+};
+
+export default Footer;
