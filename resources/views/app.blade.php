e0f956bb12895b974abaf9703ee1a37fa5f8172f
Shriyash Jadhav <shriyashjadhav370@gmail.com>
2021-05-18 10:51:02 +0530

    Title and logo added

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{config('app.name')}}</title>
+    <link rel="shortcut icon" href="images/edhub-logo.png" type="image/x-icon">
     <link rel="stylesheet" href="{{mix('css/app.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
     <link
