725e3a02100a5e57afb539ca2c91e7662170a582
Shubhra Singh <shubhra498@gmail.com>
2021-05-25 16:09:36 +0530

    Course update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
+use Livewire\LivewireBladeDirectives;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Blade::directive('this', [LivewireBladeDirectives::class, 'this']);
     }
 
     /**
