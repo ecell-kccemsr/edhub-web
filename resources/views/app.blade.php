52cd30a02b939036bdb7487946ec5d8221e93d02
Shriyash Jadhav <shriyashjadhav370@gmail.com>
2021-05-25 13:48:20 +0530

    update app blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="verify-admitad" content="1356d9a948" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{config('app.name')}}</title>
     <link rel="shortcut icon" href="images/edhub-logo.png" type="image/x-icon">
