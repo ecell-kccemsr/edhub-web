9b151c7f5c02c007e58a9b63c0215c2397a2d43f
Bhaktij Koli <bhaktijkoli121@gmail.com>
2021-05-28 14:51:52 +0530

    chore: updated clean

--- webpack.clean.js
+++ webpack.clean.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 fs.removeSync(path.join('public', 'css'))
 fs.removeSync(path.join('public', 'js'))
+fs.removeSync(path.join('public', 'fonts'))
+fs.removeSync(path.join('public', 'vendor'))
 console.log("CSS, JS Folders removed")
 
 fs.removeSync(path.join('public', 'mix-manifest.json'))
