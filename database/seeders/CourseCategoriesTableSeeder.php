9c2be8b8561b3b7420d23e84b168f3561d2e4618
prachinimase <prachinimase@gmail.com>
2021-06-11 13:28:21 +0530

    course categories and subcategories added

--- database/seeders/CourseCategoriesTableSeeder.php
+++ database/seeders/CourseCategoriesTableSeeder.php
@@ -35,6 +35,54 @@ class CourseCategoriesTableSeeder extends Seeder
                 'created_at' => '2021-06-06 22:44:14',
                 'updated_at' => '2021-06-06 22:44:14',
             ),
+            2 => 
+            array (
+                'id' => 3,
+                'name' => 'Finance & Accounting Courses',
+                'slug' => 'finance-accounting-courses',
+                'created_at' => '2021-06-11 12:31:34',
+                'updated_at' => '2021-06-11 12:31:34',
+            ),
+            3 => 
+            array (
+                'id' => 4,
+                'name' => 'IT & Software',
+                'slug' => 'it-software',
+                'created_at' => '2021-06-11 12:31:58',
+                'updated_at' => '2021-06-11 12:31:58',
+            ),
+            4 => 
+            array (
+                'id' => 5,
+                'name' => 'Office Productivity',
+                'slug' => 'office-productivity',
+                'created_at' => '2021-06-11 12:32:14',
+                'updated_at' => '2021-06-11 12:32:14',
+            ),
+            5 => 
+            array (
+                'id' => 6,
+                'name' => 'Personal Development',
+                'slug' => 'personal-development',
+                'created_at' => '2021-06-11 12:32:29',
+                'updated_at' => '2021-06-11 12:32:29',
+            ),
+            6 => 
+            array (
+                'id' => 7,
+                'name' => 'Design',
+                'slug' => 'design',
+                'created_at' => '2021-06-11 12:32:46',
+                'updated_at' => '2021-06-11 12:32:46',
+            ),
+            7 => 
+            array (
+                'id' => 8,
+                'name' => 'Marketing',
+                'slug' => 'marketing',
+                'created_at' => '2021-06-11 12:33:05',
+                'updated_at' => '2021-06-11 12:33:05',
+            ),
         ));
         
         
