887640df025de09721a9033286d80653c60b0ba8
Bhaktij Koli <bhaktijkoli121@gmail.com>
2021-06-18 09:15:06 +0530

    fix: duplicate name routes

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::view('/blog/{blog_slug}', 'app')->name('blog.show');
 
 Route::view('/govermentjobs', 'app');
 Route::view('/govermentjobs/{category_slug}/{subcategory_slug}', 'app')->name('government_job.show');
-Route::view('/govermentjobs/{category_slug}/{subcategory_slug}/{slug}', 'app')->name('government_job.show');
+Route::view('/govermentjobs/{category_slug}/{subcategory_slug}/{slug}', 'app');
 
 Route::view('/exam/calendar', 'app')->name('exam_calendar.show');
 Route::view('/courseDetail/{course_slug}', 'app')->name('course.show');
