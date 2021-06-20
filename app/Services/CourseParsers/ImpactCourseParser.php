dd05d1e7d33f00a1218f3f6ad7a021f736720c18
prachinimase <prachinimase@gmail.com>
2021-06-20 20:25:22 +0530

    Course parser added

--- app/Services/CourseParsers/ImpactCourseParser.php
+++ app/Services/CourseParsers/ImpactCourseParser.php
@@ -2,9 +2,73 @@
 
 namespace App\Services\CourseParsers;
 
-class ImpactCourseParser extends CourseParser
+use App\Models\Course;
+use App\Models\CourseInstructor;
+use App\Models\CourseTopic;
+
+class ImpactCourseParser
 {
     public function parse($file)
     {
+        $fn = fopen($file,"r");
+        $first_line = fgets($fn);
+        $keys = explode("\t",$first_line);
+        $array = [];
+        $i=0;
+        while(! feof($fn))  {
+            $line = fgets($fn);
+            $data = explode("\t",$line);
+            $data_arr = [];
+            foreach($data as $key=>$value)
+            {
+                $data_arr[rtrim($keys[$key])] = strlen(trim($value)) == 0?Null:$value;
+            }
+            $array[$i] = $data_arr;
+            $i++;
+        }
+        array_pop($array);
+        foreach($array as $a)
+        {
+            if (Course::where('cid', $a['id'])->first() !== null) {
+                continue;
+            }
+            $course = new Course();
+            if($a['id']!=null)
+            {
+                $course->cid = $a['id'];
+            }
+            if($a['title']!=null)
+            {
+                $course->title = $a['title'];
+            }
+            if($a['language']!=null)
+            {
+                $course->locale = $a['language'];
+            }
+            if($a['link']!=null)
+            {
+                $course->url = $a['link'];
+            }
+            if($a['description']!=null)
+            {
+                $course->description = $a['description'];
+            }
+            if($a['courseLevel']!=null)
+            {
+                $course->difficulty_level = $a['courseLevel'];
+            }
+            if($a['imageLink']!=null)
+            {
+                $course->image = $a['imageLink'];
+            }
+            if($a['price']!=null)
+            {
+                $course->price = floatval($a['price']);
+            }
+            $course->course_provider_id = 5;
+            $course->generateSlug();
+            $course->save();
+        }
+        fclose($fn);
     }
 }
--- app/Services/CourseParsers/RakutenCourseParser.php
+++ app/Services/CourseParsers/RakutenCourseParser.php
@@ -4,7 +4,28 @@ namespace App\Services\CourseParsers;
 
 class RakutenCourseParser extends CourseParser
 {
-    public function parse($file)
+    public static function parse($file)
     {
+        // $fileContents= file_get_contents($file);
+        // $fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
+        // $fileContents = trim(str_replace('"', "'", $fileContents));
+        $simpleXml = simplexml_load_string($file);
+        $json = json_encode($simpleXml);
+        dd($json);
+        // $result =  json_decode($json,true);
+        // print_r($result);
+        // foreach ($result as $item) {
+        //     print_r($item['product_id']);
+        //     break;
+        // }
+
+        // foreach($json as $j)
+        // {
+        //     print_r($j['merchantId']);
+        //     // break;
+
+        // }
+
+        
     }
 }
